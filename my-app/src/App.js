cc673ce535983bf25dc25f8b0143618f926835a4
cavok.ky <cavok.ky@gmail.com>
2024-04-07 16:16:16 +0900

    Add flower-actions

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './flower.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Flower
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
